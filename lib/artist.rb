c11472fef9f34a4e81eb68ea95574f1d1f5f45f5
Richard Cho <richardcho100@gmail.com>
2018-05-13 01:56:45 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+class Artist
+  attr_accessor :name
+end
+
+class Author
+  attr_accessor :name
+end
+
+class Post
+  attr_accessor :title, :author
+end
+
+class Song
+  attr_accessor :title, :artist
+end
